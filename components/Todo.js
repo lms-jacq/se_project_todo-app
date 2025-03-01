ec7e7373079b9e2df156f0d32251a9f4f5da796a
lms.jacq <mei.san.lau@gmail.com>
2025-02-28 23:39:46 -0800

    Updates for project 8

--- components/Todo.js
+++ components/Todo.js
@@ -1,15 +1,29 @@
 class Todo {
-  constructor(data, selector) {
+  constructor(data, selector, handleCheck, handleDelete) {
     this._data = data;
     this._templateElement = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
+
+    // this._completed = data.completed;
+    // this._name = data.name;
+    // this._date = data.date;
+    // this._id = data.id;
+    // this._selector = selector;
+    // this._handleCheck = handleCheck;
+    // this._handleDelete = handleDelete;
   }
 
   _setEventListeners() {
     this._todoDeleteBtn.addEventListener("click", () => {
       this._todoElement.remove();
+      this._handleDelete(this._completed);
+      this._remove();
     });
     this._todoCheckboxEl.addEventListener("change", () => {
       this._data.completed = !this._data.completed;
+      this._toggleCompletion();
+      this._handleCheck(this._completed);
     });
   }
 
@@ -51,5 +65,14 @@ class Todo {
     return this._todoElement;
   }
 }
+
+_toggleCompletion = () => {
+  this._completed = !this._completed;
+};
+
+_remove = () => {
+  this._todoElement.remove();
+  this._todoElement = null;
+};
 
 export default Todo;
